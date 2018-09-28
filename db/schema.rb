ca0261dfc18649c0b3a18e84d81c2b91d48644f9
Liina Püttsepp <liina.rebassoo@khk.ee>
2018-09-28 13:27:24 +0300

    Create relations between event and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_074111) do
+ActiveRecord::Schema.define(version: 2018_09_28_091727) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_074111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "featured", default: false
+    t.integer "category_id"
   end
 
   create_table "models", force: :cascade do |t|
