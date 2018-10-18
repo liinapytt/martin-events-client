1c8094976127be01ae03082a727a25a361b79067
Liina Püttsepp <liina.rebassoo@khk.ee>
2018-10-18 16:36:04 +0300

    haiku api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ApplicationController
 	skip_before_action :authenticate_user!
+	skip_before_action :verify_authenticity_token
   def about
   	render json: {
   		name: 'liina',
@@ -7,5 +8,21 @@ class ApiController < ApplicationController
   		time: Time.now.strftime("%H:%M"),
   		fruits: ['pineapple', 'apple', 'pear', 'grapefruit', 'watermelon']
   		}
+  end
+
+  def haiku
+  	render json: {
+  		haiku: reverse(haiku_params[:haiku]),
+  		name: haiku_params[:name].upcase
+  	}
+  end
+
+  def haiku_params 
+  	params.permit(:haiku, :name)
+	 
+  end
+
+  def reverse(haiku) 
+  	haiku.split(' ').reverse.join(' ')  
   end
 end
